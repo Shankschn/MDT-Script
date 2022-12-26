e015be0845e14525d85a9b89677c542b22b73df2
Shanks <63188620+Shankschn@users.noreply.github.com>
2022-12-26 18:48:41 +0800

    Add files via upload

---
+++ clear_disk_gpt.ps1
@@ -0,0 +1,4 @@
+﻿Get-Disk |ForEach {
+    Clear-Disk -Number $_.Number -RemoveData -RemoveOEM -Confirm:$false
+}
+Get-Disk |Where-Object PartitionStyle -eq 'RAW' |Initialize-Disk -PassThru |New-Partition -AssignDriveLetter -UseMaximumSize |Format-Volume -FileSystem NTFS -Confirm:$false
