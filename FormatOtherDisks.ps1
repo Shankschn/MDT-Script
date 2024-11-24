f3b253ec5689c56ba8a62941619fafa5fd814593
Shanks <63188620+Shankschn@users.noreply.github.com>
2024-11-24 15:02:08 +0800

    Update FormatOtherDisks.ps1

--- FormatOtherDisks.ps1
+++ FormatOtherDisks.ps1
@@ -1,7 +1,6 @@
 Get-WmiObject -Class Win32_Volume -Filter "DriveType=5 and DriveLetter='D:'" |Select-Object -First 1 |Set-WmiInstance -Arguments @{DriveLetter="R:"}
 Get-Disk |Where-Object PartitionStyle -eq 'RAW' |Initialize-Disk -PassThru |New-Partition -UseMaximumSize |Format-Volume -FileSystem NTFS -Confirm:$false
 $tmp1 = Get-Partition | Where-Object {-not $_.DriveLetter -and $($_.size/1024/1024/1024) -gt 30}
-Write-Host $tmp1
 foreach ($tmp2 in $tmp1) {
     $tmp3 = Get-Volume | Where-Object {$_.DriveLetter -ne "R" -and -not -not $_.DriveLetter} |Sort-Object DriveLetter |Select-Object -Last 1
     $tmp4 = [char]([int][char]$tmp3.DriveLetter + 1)
